2ad3b106eb313d7b045231675af763f8f7c87ba4
Олег Сайфутдинов <zeusfly@mail.ru>
2023-05-27 23:44:06 +0300

    Added nuxt front directory

---
+++ front/pages/login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <form @submit.prevent="userLogin">
+      <div>
+        <label>Username</label>
+        <input type="text" v-model="login.email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="text" v-model="login.password" />
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  data: () =>({
+    login: {
+      email: '',
+      password: ''
+    }
+  }),
+  methods: {
+    async userLogin() {
+      try {
+        let response = await this.$auth.loginWith('local', { data: this.login })
+        console.log(response)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
